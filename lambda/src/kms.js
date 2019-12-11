d4fc21de8db35c9259bafddffafa1d2639a5fc83
Rahman Younus <rahman.younus@dazn.com>
2019-12-11 17:45:57 +0000

    wip - refactor kms stuff, needs testing

--- lambda/src/kms.js
+++ lambda/src/kms.js
@@ -1,34 +1,20 @@
 // https://github.com/aws/aws-sdk-js-v3/tree/master/clients/node/client-kms-node
 
-// FIX ME
-
-const AWS = require("aws-sdk");
-AWS.config.update({ region: "eu-west-2" });
+const { KMSClient } = require("@aws-sdk/client-kms-node");
 
 const encrypted = process.env["ACCESS_KEY"];
-let decrypted;
 
-function processEvent(event, context, callback) {
-  // TODO handle the event here
+function decryptValue(encrypted)
+{
+  try {
+    const kms = new KMSClient();
+    const res = await kms.decrypt({
+      CiphertextBlob: new Buffer(encrypted, "base64")
+    }).promise();
+  
+    return res.Plaintext.toString();
+  } catch(err) {
+    throw new Error(err.message)
+  }
 }
 
-exports.handler = (event, context, callback) => {
-  if (decrypted) {
-    processEvent(event, context, callback);
-  } else {
-    // Decrypt code should run once and variables stored outside of the
-    // function handler so that these are decrypted once per container
-    const kms = new AWS.KMS();
-    kms.decrypt(
-      { CiphertextBlob: new Buffer(encrypted, "base64") },
-      (err, data) => {
-        if (err) {
-          console.log("Decrypt error:", err);
-          return callback(err);
-        }
-        decrypted = data.Plaintext.toString("ascii");
-        processEvent(event, context, callback);
-      }
-    );
-  }
-};
